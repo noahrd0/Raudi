966850ec0959059c667ee7263c77144bbb9c711f
noahrd0 <noahradal636@gmail.com>
2024-01-31 16:47:05 +0100

    init Raudi BDD

---
+++ modeles/modele.js
@@ -0,0 +1,35 @@
+const sequelize = require('../database/database');
+const DataTypes = require('sequelize');
+
+const Modele = sequelize.define('modele', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    nom: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    portes: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    places: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    carburant : {
+        type: DataTypes.ENUM('essence', 'diesel', 'electrique'),
+        allowNull: false
+    },
+    prix: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    }
+}, {
+    freezeTableName: true
+});
+
+module.exports = Modele;
---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const sequelize = require('./database/database');
+const cors = require('cors');
+
+const voiture = require('./routes/VoitureRoute');
+
+app.use(cors());
+app.use(express.json());
+app.use('/raudi', voiture);
+
+app.listen(8000, () => {
+    console.log('Server port 8000');
+    sequelize.sync();
+});
